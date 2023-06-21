3c53bf2756fd40231234eef6166c6b2e3d41e0c4
arif3ana <ariftriana28@gmail.com>
2023-06-21 17:41:14 +0700

    feat: -D gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./style/App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
 import Archive from "./pages/Archive";
 
 function App() {
