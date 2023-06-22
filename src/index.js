8fcb1e6fe6c3a2036eed32cccc75586b67ae5109
arif3ana <ariftriana28@gmail.com>
2023-06-22 10:31:09 +0700

    feat:bismillah

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/MyDiary'>
+    <HashRouter basename='/MyDiary/#'>
       <App />
     </HashRouter>
   </React.StrictMode>
