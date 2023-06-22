ff481c75f12ab2be72d0768148139e10d425bd38
arif3ana <ariftriana28@gmail.com>
2023-06-22 09:44:21 +0700

    feat: restore gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./style/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Archive from "./pages/Archive";
 
 function App() {
   return (
-    <BrowserRouter basename='/MyDiary'>
+    <HashRouter basename='/MyDiary'>
       <Routes>
         <Route index path='/Home' element={<Home />} />
         <Route path='/archive' element={<Archive />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
